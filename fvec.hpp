8af96bab887c5729198f0ef76f92b0f362902aae
Mark Richardson <oldkentuckymark@gmail.com>
2025-06-28 02:11:33 -0400

    more noodling

--- fvec.hpp
+++ fvec.hpp
@@ -1,41 +1,70 @@
 #pragma once
 
+#include <cstdint>
+
+
+
+//typedef s32 fixed32;
+//typedef s16 fixed16;
+
+
+
+//static inline fixed32 fx_from_int(int d) {
+//    return d << FIX_SHIFT;
+//}
+
+// static inline fixed32 fx_from_float(float f) {
+//     return (fixed32)(f*FIX_SCALE);
+// }
+
+// static inline int fx_to_int(fixed32 fx) {
+//     return fx / FIX_SCALE;
+// }
+
+// static inline float fx_to_float(fixed32 fx) {
+//     return fx / FIX_SCALEF;
+// }
+
+// static inline fixed32 fx_multiply(fixed32 fa, fixed32 fb) {
+//     return (fa*fb)>>FIX_SHIFT;
+// }
+
+// static inline fixed32 fx_division(fixed32 fa, fixed32 fb) {
+//     return ((fa)*FIX_SCALE)/(fb);
+// }
+
+
+
 namespace fvec
 {
 
-template<class T>
-class vec2
+
+class fixed32   //24.8
 {
+static constexpr int16_t FIX_SHIFT = 8;
+static constexpr int16_t FIX_SCALE = 256;
+static constexpr float FIX_SCALEF = 256.0f;
+
 public:
-    T x,y;
+    int32_t data;
 
-    auto operator + (vec2 const & that) -> vec2
+    fixed32() = default;
+    fixed32(fixed32 const &that) = default;
+    fixed32& operator = (fixed32 &that) = default;
+
+    fixed32(int16_t that)
     {
-        return {x+that.x,y+that.y};
+
     }
 
-    auto operator - (vec2 const & that) -> vec2
+
+    operator int32_t ()
     {
-        return {x-that.x,y-that.y};
+        return
     }
 
-    auto operator * (T const scalar) -> vec2
-    {
-        return {x*scalar,y*scalar};
-    }
 
-    auto operator / (T const scalar) -> vec2
-    {
-        return {x/scalar,y/scalar};
-    }
 };
-
-template<class T>
-class vec3
-{
-    T x,y,z;
-};
-
 
 template<class TYPE,  void (*PIXEL_FUNCTION)(short,short)>
 class Renderer
